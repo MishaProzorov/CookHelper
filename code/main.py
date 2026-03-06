75b998b04424fbf69483043ee96e42b93688fa7b
Misha <mihailprozorov4@gmail.com>
2026-03-06 06:48:58 +0300

    начало работы с css и html

--- code/main.py
+++ code/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from sqlalchemy.orm import Session
-from fastapi import Depends
-from fastapi import HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Depends 
 from models import  User
+import os
 from database import engine, session, Base
 from schemas import UserClass, UserCreate
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+app.mount("/static", StaticFiles(directory=os.path.join(current_dir, "static")), name="static")
+templates = Jinja2Templates(directory=os.path.join(current_dir, "templates"))
 Base.metadata.create_all(bind=engine)
 
 def get_db():
@@ -17,8 +24,15 @@ def get_db():
     finally:
         db.close()
 
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("head.html", {"request": request})
 
-@app.post("/users/", response_model=UserClass)
+@app.get("/registration", response_class=HTMLResponse)
+def registr(request: Request):
+    return templates.TemplateResponse("registration.html", {"request": request})
+
+@app.post("/registration/", response_model=UserClass)
 def creat_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = User(
         name = user.name, 
@@ -32,13 +46,13 @@ def creat_user(user: UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.get("/users/", response_model=list[UserClass])
+@app.get("/registration/", response_model=list[UserClass])
 def return_user(db: Session=Depends(get_db)):
     users = db.query(User).all()
     return users
 
 
-@app.get("/users/{id}", response_model=UserClass)
+@app.get("/registration/{id}", response_model=UserClass)
 def return_one_user(id: int, db: Session=Depends(get_db)):
     user = db.query(User).filter(User.id == id).first()
     if user is None:
@@ -46,7 +60,7 @@ def return_one_user(id: int, db: Session=Depends(get_db)):
     return user
 
 
-@app.delete("/users/{id}", response_model=UserClass)
+@app.delete("/registration/{id}", response_model=UserClass)
 def delete_user(id: int, db: Session=Depends(get_db)):
     user = db.query(User).filter(User.id == id).first()
     if user is None:
@@ -56,7 +70,7 @@ def delete_user(id: int, db: Session=Depends(get_db)):
     return user
 
 
-@app.put("/users/{id}",response_model=UserClass)
+@app.put("/registration/{id}",response_model=UserClass)
 def change_user(id: int, new: UserCreate, db: Session=Depends(get_db)):
     user = db.query(User).filter(User.id == id).first()
     if user is None:
@@ -64,18 +78,16 @@ def change_user(id: int, new: UserCreate, db: Session=Depends(get_db)):
             status_code=404, 
             detail = "Пользователя с таким id не существует"
         )
-    user.name = new.name,
-    user.age = new.age,
-    user.password = new.password,
+    user.name = new.name
+    user.age = new.age
+    user.password = new.password
     user.gmail = new.gmail
     db.commit()
     db.refresh(user)
     return user
 
 
-@app.get("/")
-def get():
-    return "Hellow"
+
 
 # @app.get("/")
 # def get():
